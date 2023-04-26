cdfb90119b0dca6f33145115d7be6b6e5fa2626e
Artem <artem.sereda@campus.tu-berlin.de>
2023-04-26 20:32:28 +0200

    :rocket: (#5)
    * :rocket:
    
    * :rocket:
    
    * :rocket:
    
    * :rocket:
    
    * :rocket:
    
    * :rocket:
    
    ---------
    
    Co-authored-by: artem-sereda <77972398+artem-sereda@users.noreply.github.com>

---
+++ __tests__/action.test.ts
@@ -0,0 +1,20 @@
+import {expect, describe, it, jest, afterEach} from '@jest/globals'
+import * as mypy_action from '../src/action'
+import {RAW_OUTPUT, TOX_RUN_OUTPUT} from './data'
+
+afterEach(() => {
+  // restore replaced property
+  jest.restoreAllMocks()
+})
+
+describe('test parse output', function () {
+  it('raw', function () {
+    const annotations = mypy_action.parseMypyOutput(RAW_OUTPUT)
+    expect(annotations.length).toBe(15)
+  })
+
+  it('tox', function () {
+    const annotations = mypy_action.parseMypyOutput(TOX_RUN_OUTPUT)
+    expect(annotations.length).toBe(15)
+  })
+})
